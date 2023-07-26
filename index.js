dc43feb2cccc18d04a924caf15230ea97ded7258
unknown <superpegorino@gmail.com>
2023-07-26 02:48:36 -0500

    CORS options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
+import boom from "@hapi/boom";
+import cors from 'cors';
 import { routerApi } from './routes/index.js'
-
 import { logErrors, errorHandler, boomErrorHandler } from './middlewares/error.handler.js';
 
 const app = express();
@@ -8,14 +9,23 @@ const port = 3000;
 
 //middleware
 app.use(express.json());
+const whiteList = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+    origin: (origin, callback) => {
+        if (whiteList.includes(origin)){
+            callback(null, true);
+        } else {
+            callback(boom.forbidden('Domain not allowed by CORS'));
+        }
+    }
+}
 
 routerApi(app);
-
-
+app.use(cors(options));
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log('Escuchando puerto ' + port);
+    console.log('Listening puerto ' + port);
 });
